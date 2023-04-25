ac3a042cfeedd0747595dedae078952130fc2aef
LionHardy <leonardndobe@gmail.com>
2023-04-24 18:41:05 -0700

    task 0

---
+++ 0-Betty_would_be_proud.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Write a beautiful code that passes the Betty checks
+ *
+ * Return: ...
+ */
+
+int main(void)
+{
+	printf("Pair Project - Simple Shell\n");
+return (EXIT_SUCCESS);
+}
