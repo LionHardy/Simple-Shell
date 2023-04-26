734d170791272a15e462c0b89bab0be93208e679
LionHardy <leonardndobe@gmail.com>
2023-04-25 20:43:10 -0700

    Updated task 6

--- 6-simple_shell.c
+++ 6-simple_shell.c
@@ -4,62 +4,7 @@
 #define INITIAL_BUFFER_SIZE 2048
 #define BUFFER_SIZE_INCREMENT 1
 
-/**
- * leonard_getline - entry point
- *
- * Return: buffer
- */
-
-char *leonard_getline(void)
-{
-	int a, b =  0;
-	size_t size = INITIAL_BUFFER_SIZE;
-	size_t increment = BUFFER_SIZE_INCREMENT;
-	size_t threshold = 1024;
-	char *buffer = malloc(INITIAL_BUFFER_SIZE);
-
-	if (!buffer)
-	{
-		fprintf(stderr, "Error: Unable to allocate memory.\n");
-		exit(EXIT_FAILURE);
-	}
-
-	while ((a = getchar()) != EOF && a != '\n')
-	{
-		char *new_buffer;
-
-		if (b == (int)size - 1)
-		{
-			size += size < threshold ? size : increment;
-			new_buffer = realloc(buffer, size);
-
-			if (!new_buffer)
-			{
-				fprintf(stderr, "Error: Unable to allocate memory.\n");
-				free(buffer);
-				exit(EXIT_FAILURE);
-			}
-			buffer = new_buffer;
-		}
-
-		if (a == '\0')
-		{
-			fprintf(stderr, "Error: Invalid input.\n");
-			free(buffer);
-			exit(EXIT_FAILURE);
-		}
-
-		buffer[b++] = (char) a;
-	}
-
-	if (a == EOF && b == 0)
-	{
-		free(buffer);
-		return (NULL);
-	}
-	buffer[b] = '\0';
-	return (buffer);
-}
+char *leonard_getline(void);
 
 /**
  * main - Write your own getline function
